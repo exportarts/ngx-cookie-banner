a7776eb3f8c20789aab35544cdf5be8a0ca3c5dd
Enzo Volkmann <enzo@exportarts.io>
2020-01-11 11:27:40 +0100

    fix: add missing stuff to public api

--- projects/ngx-cookie-banner/src/public-api.ts
+++ projects/ngx-cookie-banner/src/public-api.ts
@@ -1,1 +1,3 @@
-export * from './lib/ngx-cookie-banner.module';
+export { NgxCookieBannerComponent } from './lib/components/ngx-cookie-banner/ngx-cookie-banner.component';
+export { NgxCookieBannerConfig, NGX_COOKIE_BANNER_CONFIG } from './lib/config';
+export { NgxCookieBannerModule } from './lib/ngx-cookie-banner.module';
